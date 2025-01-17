71f35faec606ca0c0ddc7d37accff600ce393c45
Chris Wen <s224212855@deakin.edu.au>
2025-01-17 22:26:51 +1100

    data back up

---
+++ week07/week04.sql
@@ -0,0 +1,8 @@
+INSERT INTO week04 (_id, title, description, evaluation, type) VALUES ('{"$oid":"674b89f8dde37725ad932ca4"}', 'Week01', 'learning about HTML, CSS, JavaScript, and Materialize framework.', 'good', 'card');
+INSERT INTO week04 (_id, title, description, evaluation, type) VALUES ('{"$oid":"674b89f8dde37725ad932ca6"}', 'Week03', 'combine node.js, express and materialize to create a web application demo.', 'good', 'card');
+INSERT INTO week04 (_id, firstName, lastName, email) VALUES ('{"$oid":"674bad5ce53a39ea1ada106a"}', 'Chris', 'W', 'mick@deakin.edu.au');
+INSERT INTO week04 (_id, title, description, evaluation, type) VALUES ('{"$oid":"67582d10027d9d58c8129f5b"}', 'week04', 'learning the MVC framework', 'GOOD', 'card');
+INSERT INTO week04 (_id, title, description, evaluation, type) VALUES ('{"$oid":"67583f91196fd9893df84e06"}', 'Week05', 'modificating project to MVC framework', 'GOOD', 'card');
+INSERT INTO week04 (_id, description, evaluation, type, title) VALUES ('{"$oid":"678112eabd0dbdef211ea879"}', 'learn how to apply mocha and chai in project', 'NORMAL', 'card', 'week06');
+INSERT INTO week04 (_id, title, description, evaluation, type) VALUES ('{"$oid":"678122ec317fb1fb6c86756c"}', 'Week02', 'apply express library to create a server and handle HTTP requests.', 'GOOD', 'card');
+INSERT INTO week04 (_id, title, description, evaluation, type) VALUES ('{"$oid":"678504397bcb84403a7cc415"}', 'test', 'test123', 'test123', 'card');
